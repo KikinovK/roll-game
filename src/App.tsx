92267f123137940632ec500a0a4b02b28df4fcb4
Kostaintyn Kikinov <kikinov.k@gmail.com>
2025-06-19 22:05:57 +0300

    init: set up Vite

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+const App = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      Roll Craft Game
+    </div>
+  )
+}
+
+export default App
